d808a2c6dc06690f131c18e81b7a69e1e65d46a2
Kabao Thao <KabaoKThao@gillettechildrens.com>
2022-03-01 19:42:35 -0600

    added notes

---
+++ Stopwatch/Stopwatch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stopwatch
+{
+    internal class Stopwatch
+    {
+        
+    }
+}
+
+
+/*
+It should provide two methods: Start and Stop. We call the start method first, and the stop method next. 
+Then we ask the stopwatch about the duration between start and stop. Duration should be a 
+value in TimeSpan. Display the duration on the console.
+
+ */
