b35495e2d771fc6baef79f1de6b2b9e994c923fd
Kabao Thao <KabaoKThao@gillettechildrens.com>
2022-03-01 20:20:23 -0600

    testing codes

--- Stopwatch/Program.cs
+++ Stopwatch/Program.cs
@@ -5,11 +5,37 @@ using System;
 
 namespace Stopwatch // Note: actual namespace depends on the project name.
 {
-    internal class Program
+    internal static class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string Input;
+            var Timer = new Stopwatch();
+
+            Console.WriteLine("Enter S to start the stopwatch and Q to stop.");
+            Input = Console.ReadLine();
+
+            while (!string.IsNullOrEmpty(Input))
+            {
+                if (string.Equals("s", Input) || string.Equals("S", Input))
+                {
+                    Timer.Start();
+                    Console.WriteLine("Enter Q to stop the stopwatch.");
+                }
+                else if (string.Equals("q", Input, StringComparison.CurrentCultureIgnoreCase))
+                {
+                    Timer.Stop();
+                    Console.WriteLine("Enter S to start the stopwatch.");
+                }
+
+                Input = Console.ReadLine();
+            }
+
+
+
+
+            SByte
+
         }
     }
 }
--- Stopwatch/Stopwatch.cs
+++ Stopwatch/Stopwatch.cs
@@ -8,14 +8,54 @@ namespace Stopwatch
 {
     internal class Stopwatch
     {
-        
-    }
+        private Boolean _isStart; //Used to check whether the stopwatch is running.
+        private DateTime _startTime; //_startTime and _stopTime : are DateTime objects that are used to record the start time and stop time respectively.
+        private DateTime _stopTime;
+        private TimeSpan _timeSpan; //_timeSpan is a TimeSpan object, as the name suggestes it stores the time span between start and stop times.
+
+        public void Start()
+            {
+                if (_isStart)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                _startTime = DateTime.Now;
+                _isStart = true;
+
+            }
+
+            public string Stop()
+            {
+                if (!_isStart)
+                {
+                    throw new InvalidOperationException();
+                }
+                _stopTime = DateTime.Now;
+                _timeSpan = _stopTime - _startTime;
+                _isStart = false;
+
+                return _timeSpan.Seconds.ToString();
+            }
+
+
+
+
+        }
+
+
+	
 }
 
 
 /*
-It should provide two methods: Start and Stop. We call the start method first, and the stop method next. 
-Then we ask the stopwatch about the duration between start and stop. Duration should be a 
-value in TimeSpan. Display the duration on the console.
+i) Read the input from the user.
+ii) If the input is start -> start the stopwatch
+If the input is stop -> throw an InvalidOperation exception
+iii) Read another input from the user
+iv) If the input is stop -> stop the stopwatch and print the time span
+If the input is start -> throw an InvalidOperation exception
+v) Repeat from step (i).
+
 
  */
